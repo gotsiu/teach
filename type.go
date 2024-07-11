9551e972e31b6b85cee43b0e0b7ec3acca88bf24
gotsiu <isme3x@outlook.com>
2024-07-11 18:11:17 +0800

    Modify type.go

--- type.go
+++ type.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# History understand walk writer those town.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
