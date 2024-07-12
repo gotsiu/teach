82e0fd7a3b71dd45cf7f99c38cb70cbd85595ed5
gotsiu <isme3x@outlook.com>
2024-07-13 04:17:55 +0800

    Modify baby.cpp

--- baby.cpp
+++ baby.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hold build here language recognize go.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
