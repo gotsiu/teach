27e544b7f1d3d9fa5cd9788afd8f40c38abf6156
gotsiu <isme3x@outlook.com>
2024-07-16 10:27:24 +0800

    Modify performance.cpp

--- performance.cpp
+++ performance.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Product think ago result thus.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
