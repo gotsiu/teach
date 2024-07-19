41b35fd688f620d0ae456597b8ead225cac1027c
gotsiu <isme3x@outlook.com>
2024-07-19 12:04:09 +0800

    Modify performance.cpp

--- performance.cpp
+++ performance.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Actually design against difficult sport large safe soldier.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
