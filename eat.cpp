dea473686b84b5824ab49874e52a789dd0c2de65
gotsiu <isme3x@outlook.com>
2024-07-19 12:04:09 +0800

    Modify eat.cpp

--- eat.cpp
+++ eat.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ten response side economic.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
