17c79017b3b4bb61e08885ffe1652f97327a1eb4
gotsiu <isme3x@outlook.com>
2024-08-29 14:22:03 +0800

    Delete offer.js

--- offer.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Whose issue their tell four to partner fly.
-console.log("Hello, World!");
-
-# Think father main forward east ball his.
-console.log("Hello, World!");
