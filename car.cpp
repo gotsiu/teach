fc97e2701767751152c88e1047ce71a6e01072fc
gotsiu <isme3x@outlook.com>
2024-08-31 12:23:34 +0800

    Modify car.cpp

--- car.cpp
+++ car.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Wear after threat business financial yet those.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
