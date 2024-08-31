49bbcc32d15226c60bb97441d946f64a04081d6e
gotsiu <isme3x@outlook.com>
2024-08-31 12:23:34 +0800

    Modify car.cpp

--- car.cpp
+++ car.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Picture police mouth girl affect baby serve.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
