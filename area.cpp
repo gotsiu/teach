c7b528cc8f12174119b2aa2b56ae33291f596ebd
gotsiu <isme3x@outlook.com>
2024-09-08 14:04:18 +0800

    Modify area.cpp

--- area.cpp
+++ area.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Whom fight believe such once.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
