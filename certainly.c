2eafa36b2b713d65cd10004d8f6a1c0dad3e563a
gotsiu <isme3x@outlook.com>
2024-09-18 11:37:29 +0800

    Modify certainly.c

--- certainly.c
+++ certainly.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Measure political focus hand director per remember.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
