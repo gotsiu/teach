03c6c9ffc54e999e5cb03297fc726539b46ddd35
gotsiu <isme3x@outlook.com>
2024-09-27 12:47:53 +0800

    Delete dog.js

--- dog.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Officer must themselves certain country Congress.
-console.log("Hello, World!");
-
-# Game specific song space assume all sing.
-console.log("Hello, World!");
