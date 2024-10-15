fe0cea0a573d8e60a587893005ade991fa3b3916
gotsiu <isme3x@outlook.com>
2024-10-15 11:02:23 +0800

    Modify trip.cpp

--- trip.cpp
+++ trip.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Time difficult white eye.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
