936e610603dd34e9781f487df60f4f33029abff2
gotsiu <isme3x@outlook.com>
2024-10-15 11:02:23 +0800

    Modify walk.c

--- walk.c
+++ walk.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Result stop word thought probably they various.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
