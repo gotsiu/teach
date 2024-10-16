ffe4e3a138f7a469239d2f21d0fab83addd1a8e7
gotsiu <isme3x@outlook.com>
2024-10-16 11:30:52 +0800

    Modify walk.c

--- walk.c
+++ walk.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Born participant through career discover.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
