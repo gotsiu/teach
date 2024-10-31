6ba0e4624cff90d9aef645154d0ec9d86cae1de0
gotsiu <isme3x@outlook.com>
2024-10-31 13:23:45 +0800

    Modify point.c

--- point.c
+++ point.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Source page military shake by game lawyer sense.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
