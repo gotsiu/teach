8e5ad864f2f6aa28efbf1692c798226eb9954475
gotsiu <isme3x@outlook.com>
2024-11-07 00:57:28 +0800

    Modify sing.go

--- sing.go
+++ sing.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Ability agreement over parent community.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
