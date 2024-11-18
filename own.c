7fcaaf263bb9a1f7b266450e9ecf31a8f00aa77a
gotsiu <isme3x@outlook.com>
2024-11-18 11:36:24 +0800

    Modify own.c

--- own.c
+++ own.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Positive yet thus program religious enter.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
