c25d7cbcda15f8b4dcb00793f01e61a9b82289d3
gotsiu <isme3x@outlook.com>
2024-11-20 14:33:40 +0800

    Modify own.c

--- own.c
+++ own.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Few human similar none particular church.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
