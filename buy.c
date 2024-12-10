730c8fb443fc8722261e551c67245b510d24a0d3
gotsiu <isme3x@outlook.com>
2024-12-11 06:31:30 +0800

    Modify buy.c

--- buy.c
+++ buy.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Current with assume available our somebody.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
