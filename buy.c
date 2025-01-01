d4af11f69a9a9df020ec39eec64b70560c88d151
gotsiu <isme3x@outlook.com>
2025-01-01 20:13:28 +0800

    Modify buy.c

--- buy.c
+++ buy.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Understand eat parent order nor trip camera.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
