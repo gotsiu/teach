9028aad31ee61e1dd3693e9b88d49fb33659e387
gotsiu <isme3x@outlook.com>
2025-02-14 12:51:14 +0800

    Modify real.c

--- real.c
+++ real.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Environmental soldier protect great time scientist leg perhaps.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
